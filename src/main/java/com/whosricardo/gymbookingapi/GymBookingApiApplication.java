a66f3a68dc0ef0d751ea0e38af02239127389515
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2025-12-09 17:42:37 -0300

    First Commit

---
+++ src/main/java/com/whosricardo/gymbookingapi/GymBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.whosricardo.gymbookingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBookingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymBookingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whosricardo/gymbookingapi/GymBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whosricardo.gymbookingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBookingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
