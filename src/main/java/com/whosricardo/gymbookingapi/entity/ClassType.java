27297c96cd09f22bb5d84dc01c13c6524b7f4ebb
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2025-12-11 15:19:22 -0300

    ClassType Entity Created

---
+++ src/main/java/com/whosricardo/gymbookingapi/entity/ClassType.java
@@ -0,0 +1,25 @@
+package com.whosricardo.gymbookingapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor @AllArgsConstructor
+public class ClassType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private int durationInMinutes;
+
+    private Integer maxCapacity;
+}
