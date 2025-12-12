d652f90d8be2f5dd2ccf58b7b2888adf32bf4048
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2025-12-12 16:43:13 -0300

    Created new entity (Trainer)

---
+++ src/main/java/com/whosricardo/gymbookingapi/entity/Trainer.java
@@ -0,0 +1,30 @@
+package com.whosricardo.gymbookingapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class Trainer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String specialty;
+
+    private String bio;
+
+    private String phone;
+
+    private boolean isActive;
+}
