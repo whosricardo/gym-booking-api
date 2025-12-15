678ff884a36eaf1b4bfce2022b78cc721a27f75b
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2025-12-15 16:08:29 -0300

    ClassSession entity implemented

---
+++ src/main/java/com/whosricardo/gymbookingapi/entity/ClassSession.java
@@ -0,0 +1,32 @@
+package com.whosricardo.gymbookingapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class ClassSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trainer_id", nullable = false)
+    private Trainer trainer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_type_id", nullable = false)
+    private ClassType classType;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    private Integer capacity;
+
+    private boolean active = true;
+}
