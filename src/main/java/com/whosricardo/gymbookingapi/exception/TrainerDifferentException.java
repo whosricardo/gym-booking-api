844ce85fa2d1ace62a8eaa562e7b55ee7dfbb1dc
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2025-12-15 15:01:11 -0300

    Created new exception to be used later on TrainerController

---
+++ src/main/java/com/whosricardo/gymbookingapi/exception/TrainerDifferentException.java
@@ -0,0 +1,7 @@
+package com.whosricardo.gymbookingapi.exception;
+
+public class TrainerDifferentException extends RuntimeException {
+    public TrainerDifferentException(String message) {
+        super(message);
+    }
+}
