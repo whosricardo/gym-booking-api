3cd58eb0fee375f5bcd290ab5da4232ce07d356f
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2026-01-05 16:16:49 -0300

    register http created

---
+++ src/main/java/com/whosricardo/gymbookingapi/controller/auth/AuthController.java
@@ -0,0 +1,24 @@
+package com.whosricardo.gymbookingapi.controller.auth;
+
+import com.whosricardo.gymbookingapi.dto.UserRegisterRequest;
+import com.whosricardo.gymbookingapi.dto.UserResponse;
+import com.whosricardo.gymbookingapi.mapper.UserMapper;
+import com.whosricardo.gymbookingapi.service.auth.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse register(@RequestBody UserRegisterRequest userRegisterRequest) {
+        return this.authService.register(userRegisterRequest);
+    }
+}
