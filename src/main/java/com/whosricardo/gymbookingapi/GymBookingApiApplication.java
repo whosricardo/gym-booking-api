9b051e27dd537e72d4a80110bbfabb6636ffa529
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2026-01-07 16:36:10 -0300

    application.properties security jwt, config jwtproperties

--- src/main/java/com/whosricardo/gymbookingapi/GymBookingApiApplication.java
+++ src/main/java/com/whosricardo/gymbookingapi/GymBookingApiApplication.java
@@ -1,9 +1,12 @@
 package com.whosricardo.gymbookingapi;
 
+import com.whosricardo.gymbookingapi.config.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class GymBookingApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(GymBookingApiApplication.class, args);
---
+++ src/main/java/com/whosricardo/gymbookingapi/config/security/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.whosricardo.gymbookingapi.config.security;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "security.jwt")
+@Getter
+@Setter
+public class JwtProperties {
+    private String secret;
+    private long expirationMinutes;
+}
